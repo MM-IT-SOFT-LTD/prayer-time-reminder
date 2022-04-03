a582ac67cc03738ba91439b133583ed626f1da25
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-04-03 22:20:39 +0600

    Working on setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
+import CogButton from "./components/CogButton"
+import Watch from "./components/Watch"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="min-h-screen w-full flex justify-center flex-col items-center">
-      <h1 className="text-3xl">
-        I am in center of page
-      </h1>
-      <button onClick={() => setCount(count + 1)} className="p-2 mt-4 text-blue-500 font-semibold  rounded border border-blue-500">
-        Count {count}
-      </button>
+      <CogButton />
+      <Watch />
     </div>
   )
 }
---
+++ src/components/CogButton.tsx
@@ -0,0 +1,14 @@
+import Cog from "./icons/Cog";
+
+
+export default function CogButton() {
+    return (
+        <div className="absolute flex top-4 right-4">
+            <div className="w-64">
+                <div className="w-full flex flex-end">
+                    <Cog className="h-12 w-12 opacity-10 hover:opacity-100 cursor-pointer transition duration-300 text-gray-500 ml-auto" />
+                </div>
+            </div>
+        </div>
+    )
+}
